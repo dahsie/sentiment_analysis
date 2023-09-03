e5d9d7ca004d42c05f08822cfb64d059fb2cba75
dahsie <dahsie0653@gmail.com>
2023-09-03 18:31:14 +0200

    exemple.py

---
+++ src/exemple.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import nltk 
+
+
+
+
+def main():
+    """
+    Main function to execute differents others methods of functions
+    """
+    pass
+
+
+if __name__=="__main__":
+    main()
